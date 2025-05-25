2035a51d3108f99f7aec881da2ddcfd91f62038c
eahc00 <eahc00@naver.com>
2025-05-25 21:04:59 +0900

    first commit

---
+++ src/main/java/com/sharehome/sharehome/SharehomeApplication.java
@@ -0,0 +1,13 @@
+package com.sharehome.sharehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharehome/sharehome/SharehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharehome.sharehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
